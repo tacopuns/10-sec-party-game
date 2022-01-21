fe62cc814afd4a36271b387d9fd857f356d6126b
gracie <73323714+tacopuns@users.noreply.github.com>
2022-01-20 23:34:08 -0500

    Added base
    Imported assets and created simple scene

---
+++ 10-sec-party-game/Assets/Scripts/PlayerBunny.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBunny : MonoBehaviour
+{
+   public float speed;
+
+   private Rigidbody2D rb;
+
+   private Vector2 moveVelocity;
+
+   void Start()
+   { 
+       rb = GetComponent<Rigidbody2d>();
+   }
+   void Update()
+   {
+       Vector2 moveInput = new Vector2(moveInput.GetAxis("Horizontal"), moveInput.GetAxis("Vertical"));
+       moveVelocity = moveInput.normalized * speed;
+
+   }
+
+   void FixedUpdate ()
+   {
+       rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+   }
+}
