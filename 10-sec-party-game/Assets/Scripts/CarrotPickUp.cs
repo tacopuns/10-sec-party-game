41271583667fad6bc3b08edae4387cb1bb7a006a
gracie <73323714+tacopuns@users.noreply.github.com>
2022-01-23 13:17:21 -0500

    Basic code implemented
    Functional pick up and scoring added

---
+++ 10-sec-party-game/Assets/Scripts/CarrotPickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotPickUp : MonoBehaviour
+{
+
+    private PlayerBunny Player;
+
+    public int scoreValue;
+
+    void Start()
+    {
+        Player = FindObjectOfType<PlayerBunny> ();
+    }
+   
+   
+   void OnTriggerEnter2D(Collider2D other)
+   {
+       if (other.tag == "Player")
+       {
+           Destroy(gameObject);
+           Player.collectCarrot(scoreValue);
+       }
+   }
+
+
+}
--- 10-sec-party-game/Assets/Scripts/PlayerBunny.cs
+++ 10-sec-party-game/Assets/Scripts/PlayerBunny.cs
@@ -6,23 +6,50 @@ public class PlayerBunny : MonoBehaviour
 {
    public float speed;
 
-   private Rigidbody2D rb;
+   private Rigidbody2D rb2D;
 
    private Vector2 moveVelocity;
 
+   public int carrotNumber;
+
+
    void Start()
    { 
-       rb = GetComponent<Rigidbody2d>();
+       rb2D = gameObject.GetComponent<Rigidbody2D>();
+
    }
    void Update()
    {
-       Vector2 moveInput = new Vector2(moveInput.GetAxis("Horizontal"), moveInput.GetAxis("Vertical"));
+       Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
        moveVelocity = moveInput.normalized * speed;
 
    }
 
    void FixedUpdate ()
    {
-       rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+       rb2D.MovePosition(rb2D.position + moveVelocity * Time.fixedDeltaTime);
    }
+
+   /*private void OnTriggerEnter(Collider other)
+   {
+       if (other.tag == "carrotPickup")
+       {
+           carrotNumber = carrotNumber + 1;
+           Destroy(other.gameObject);
+       }
+
+       else
+       {
+
+       }
+
+       Debug.Log("Carrot" + other);
+
+   }*/
+
+   public void collectCarrot(int totalCarrots)
+   {
+       carrotNumber += totalCarrots;
+   }
+
 }
